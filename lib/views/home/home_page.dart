521845013571555a36d641aa1ed07f6a9a6cf956
qminhminh <hqminh050503@gmail.com>
2024-11-06 22:53:23 +0700

    add launcher icons and update AndroidManifest

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:foodly_admin/common/custom_appbar.dart';
